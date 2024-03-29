8ee738666aae758b15fe7dc4ef7ff67224058c28
HollowHorizon <78298979+HollowHorizon@users.noreply.github.com>
2024-03-29 20:24:14 +0300

    Update PostEffect.kt

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/util/PostEffect.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/util/PostEffect.kt
@@ -3,12 +3,12 @@ package ru.hollowhorizon.hollowengine.common.scripting.story.nodes.util
 import ru.hollowhorizon.hc.client.utils.rl
 
 object PostEffect {
-    val SEPIA = "hc:shaders/post/sepia.json".rl
-    val GRAY = "hc:shaders/post/gray.json".rl
-    val SHAKE = "hc:shaders/post/shake.json".rl
-    val VIGNETTE = "hc:shaders/post/vig.json".rl
-    val GLITCH = "hc:shaders/post/glitch.json".rl
-    val BLUR = "shaders/post/blur.json".rl
+    val SEPIA = "hc:shaders/post/sepia.post.json".rl
+    val GRAY = "hc:shaders/post/gray.post.json".rl
+    val SHAKE = "hc:shaders/post/shake.post.json".rl
+    val VIGNETTE = "hc:shaders/post/vig.post.json".rl
+    val GLITCH = "hc:shaders/post/glitch.post.json".rl
+    val BLUR = "shaders/post/blur.post.json".rl
 
     fun create(string: String) = string.rl
 }
