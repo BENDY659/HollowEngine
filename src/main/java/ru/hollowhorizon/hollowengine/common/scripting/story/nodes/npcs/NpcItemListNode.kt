3ec4fbfcafa1d372c9516cbae4a329d266c85b88
HollowHorizon <artemm@xaker.ru>
2024-04-03 11:11:37 +0300

    collect items task

--- src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/npcs/NpcItemListNode.kt
+++ src/main/java/ru/hollowhorizon/hollowengine/common/scripting/story/nodes/npcs/NpcItemListNode.kt
@@ -7,6 +7,7 @@ import net.minecraft.world.item.ItemStack
 import ru.hollowhorizon.hc.client.utils.mcTranslate
 import ru.hollowhorizon.hollowengine.client.screen.overlays.DrawMousePacket
 import ru.hollowhorizon.hollowengine.common.entities.NPCEntity
+import ru.hollowhorizon.hollowengine.common.scripting.story.nodes.IContextBuilder
 import ru.hollowhorizon.hollowengine.common.scripting.story.nodes.Node
 
 open class GiveItemList {
@@ -16,6 +17,35 @@ open class GiveItemList {
     operator fun ItemStack.unaryPlus() {
         items.add(this)
     }
+}
+
+fun IContextBuilder.collectItems(block: GiveItemList.() -> Unit) = +ItemListNode(block)
+
+
+class ItemListNode(itemList: GiveItemList.() -> Unit) : Node() {
+    private val itemList by lazy { GiveItemList().apply(itemList) }
+
+    override fun tick(): Boolean {
+        var hasItems = false
+
+        manager.team.onlineMembers.forEach { member ->
+            hasItems = hasItems ||
+                    itemList.items.all { item ->
+                        member.inventory.items.any { it.isItemStackEqual(item) }
+                    }
+        }
+
+        return !hasItems
+    }
+
+    override fun serializeNBT() = CompoundTag()
+    override fun deserializeNBT(nbt: CompoundTag) {}
+}
+
+private fun ItemStack.isItemStackEqual(it: ItemStack): Boolean {
+    if (this.item != it.item) return false
+    if (this.count != it.count) return false
+    return true
 }
 
 class NpcItemListNode(itemList: GiveItemList.() -> Unit, npcConsumer: NPCProperty) : Node() {
